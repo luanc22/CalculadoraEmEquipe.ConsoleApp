31c33ab9ac43ce36e8b36d92122b89f85563a5dd
Luan Vale <luancabral5566@gmail.com>
2022-06-15 15:31:20 -0300

    feat: implementado o historico da calculadora.

--- CalculadoraEmEquipe.ConsoleApp/Program.cs
+++ CalculadoraEmEquipe.ConsoleApp/Program.cs
@@ -10,59 +10,97 @@ namespace CalculadoraEmEquipe.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculadora 1.0");
 
-            double primeiroNumero;
-            double segundoNumero;
+            double primeiroNumero = 0;
+            double segundoNumero = 0;
             int operacaoEscolhida;
             double resultado = 0;
+            List<string> historicoDeOperacoes = new List<string>();
+            string salvarOperacao = "";
+            bool fecharApp = false;
+            string primeiroNumeroString = "";
+            string segundoNumeroString = "";          
 
-            /*
-             * 1 - adicao
-             * 2 - subtracao
-             * 3 - multi
-             * 4 - divisao
-             * 5 - historico
-             * qualquer outro sair
-             */
-
-            Console.WriteLine("Escolha uma operação: ");
-            string opcaoString = Console.ReadLine();
-            operacaoEscolhida = int.Parse(opcaoString);
-
-            Console.Clear();
-
-            Console.WriteLine("Digite o primeiro numero: ");
-            string primeiroNumeroString = Console.ReadLine();
-            primeiroNumero = int.Parse(primeiroNumeroString);
-
-            Console.WriteLine("Digite o Segundo numero: ");
-            string segundoNumeroString = Console.ReadLine();
-            segundoNumero = int.Parse(segundoNumeroString);
-
-
-            switch (operacaoEscolhida)
+            while (fecharApp == false)
             {
-                case 1:
-                    resultado = primeiroNumero + segundoNumero;
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    resultado = primeiroNumero * segundoNumero;
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
+                Console.WriteLine("Bem vindo a Calculadora 1.0");
+                Console.WriteLine();
+
+                /*
+                 * 1 - adicao
+                 * 2 - subtracao
+                 * 3 - multi
+                 * 4 - divisao
+                 * 5 - historico
+                 * qualquer outro sair
+                 */
+
+                Console.Write("Escolha uma operação: ");
+                string opcaoString = Console.ReadLine();
+                operacaoEscolhida = int.Parse(opcaoString);
+
+                Console.Clear();
+
+                if (operacaoEscolhida != 5)
+                {
+                    Console.Write("Digite o primeiro numero: ");
+                    primeiroNumeroString = Console.ReadLine();
+                    primeiroNumero = int.Parse(primeiroNumeroString);
+
+                    Console.Write("Digite o Segundo numero: ");
+                    segundoNumeroString = Console.ReadLine();
+                    segundoNumero = int.Parse(segundoNumeroString);
+                }
+               
+                switch (operacaoEscolhida)
+                {
+                    case 1:
+                        resultado = primeiroNumero + segundoNumero;
+                        salvarOperacao = primeiroNumeroString + " + " + segundoNumero + " = " + resultado.ToString();
+                        break;
+                    case 2:
+                        salvarOperacao = primeiroNumeroString + " - " + segundoNumero + " = " + resultado.ToString();
+                        break;
+                    case 3:
+                        resultado = primeiroNumero * segundoNumero;
+                        salvarOperacao = primeiroNumeroString + " * " + segundoNumero + " = " + resultado.ToString();
+                        break;
+                    case 4:
+                        salvarOperacao = primeiroNumeroString + " / " + segundoNumero + " = " + resultado.ToString();
+                        break;
+                    case 5:
+                        Console.WriteLine("Historico de Operacoes.");
+                        Console.WriteLine();
+
+                        mostrarHistorico(historicoDeOperacoes);
+                        break;
+                }
+
+                if(operacaoEscolhida != 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Resultado fica: {0}.", resultado);
+                    historicoDeOperacoes.Add(salvarOperacao);
+                    
+                }
+
+                Console.WriteLine();
+                Console.Write("Aperte ENTER para continuar.");
+                Console.ReadLine();
+                Console.Clear();
             }
 
-            Console.WriteLine("Resultado fica: {0}.", resultado);
-            Console.WriteLine();
-            Console.WriteLine("Aperte ENTER para sair.");
-            Console.ReadLine();
+            #region Metodos.
+            void mostrarHistorico(List<string> listaDeResultados)
+            {
+                int i = 0;
+               foreach (var resultadoGravado in listaDeResultados){
+                    i++;
+                    Console.WriteLine("{0}. {1}", i, resultadoGravado);
+                    Console.WriteLine();
+               }
+            }
+            #endregion
         }
     }
 }
